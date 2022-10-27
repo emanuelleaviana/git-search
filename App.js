d7b1f82219e061222a73485d2c2beb8ff7bfdc93
Emanuelle Viana <manuavbr@gmail.com>
2022-10-27 17:16:21 -0300

    updateProfileScreen

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import { View, Text, StyleSheet, Modal, TouchableOpacity, Button } from 'react-n
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { TextInput } from 'react-native-gesture-handler';
-import { FontAwesome } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome, FontAwesome5, Entypo, AntDesign, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 function HomeScreen({ navigation }) {
@@ -20,7 +19,6 @@ function HomeScreen({ navigation }) {
             transparent={true}
             visible={modalVisible}
             onRequestClose={() => {
-              Alert.alert("Modal has been closed.");
               setModalVisible(!modalVisible);
             }}
           >
@@ -71,15 +69,59 @@ function Profile({ navigation }) {
 
         </Modal>
         <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.modal}>
-          <FontAwesome style={styles.iconSearch} name="search" size={24} color="white" />
+          <FontAwesome style={styles.iconSearch} name="search" size={28} color="white" />
         </TouchableOpacity>
       </View>
-      <View style={styles.backgroundButtons}>
-        <TouchableOpacity onPress={() => navigation.navigate('Bio')}></TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Orgs')}></TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Repositórios')}></TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Seguidores')}></TouchableOpacity>
+
+      <Text style={styles.nameProfile}>Emanuelle Viana</Text>
+      <Text style={styles.nicknameProfile}>@emanuelleaviana</Text>
+
+      <View>
+        <TouchableOpacity style={[styles.individualButton, {borderTopStartRadius:20, borderTopEndRadius:20}]} onPress={() => navigation.navigate('Bio')}>
+          <View style={styles.iconButton}><FontAwesome5 name="user" size={28} color="black" /></View>
+          <View>
+            <Text style={styles.textButtons}>Bio</Text>
+            <Text style={styles.subtitleButton}>Um pouco sobre o usuário</Text>
+          </View>
+          <Text style={[styles.iconArrowButton, {marginLeft:100}]}> <AntDesign name="right" size={24} color="black" /> </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.individualButton} onPress={() => navigation.navigate('Orgs')}>
+          <View style={styles.iconButton}>
+            <FontAwesome5 name="headset" size={28} color="black" />
+          </View>
+          <View>
+            <Text style={styles.textButtons}>Orgs</Text>
+            <Text style={styles.subtitleButton}>Organizações que o usuário faz parte</Text>
+          </View>
+          <Text style={[styles.iconArrowButton, {marginLeft:30}]}> <AntDesign name="right" size={24} color="black" /> </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.individualButton} onPress={() => navigation.navigate('Repositórios')}>
+          <View style={styles.iconButton}><Ionicons name="document-text-outline" size={28} color="black" /></View>
+          <View>
+            <Text style={styles.textButtons}>Repositórios</Text>
+            <Text style={styles.subtitleButton}>Lista contendo todos os repositórios</Text>
+          </View>
+          <Text style={[styles.iconArrowButton, {marginLeft:32}]}> <AntDesign name="right" size={24} color="black" /> </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.individualButton, {borderBottomStartRadius:20, borderBottomEndRadius:20}]} onPress={() => navigation.navigate('Seguidores')}>
+          <View style={styles.iconButton}><MaterialCommunityIcons name="face-recognition" size={28} color="black" /></View>
+          <View>
+            <Text style={styles.textButtons}>Seguidores</Text>
+            <Text style={styles.subtitleButton}>Lista de seguidores</Text>
+          </View>
+          <Text style={[styles.iconArrowButton, {marginLeft:138}]}> <AntDesign name="right" size={24} color="black" /> </Text>
+        </TouchableOpacity>
       </View>
+
+      <View style={styles.backgroundResetButton}>
+        <TouchableOpacity style={styles.resetButton}>
+          <Text style={{fontSize:16}}> <Entypo name="log-out" size={20} color="black" /> Resetar</Text>
+        </TouchableOpacity>
+      </View>
+      
     </View>
 
 
@@ -126,8 +168,8 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="Bio" component={Bio} />
         <Stack.Screen name="Orgs" component={OrgsPage} />
         <Stack.Screen name="Repositórios" component={RepositoryPage} />
@@ -135,8 +177,6 @@ function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
-
-
 }
 
 const styles = StyleSheet.create({
@@ -148,14 +188,14 @@ const styles = StyleSheet.create({
   conteinerProfile: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     flexDirection: 'column',
   },
   profilePicture: {
     borderRadius: 80,
     width: 200,
     height: 200,
-    backgroundColor: '#341933',
+    backgroundColor: '#50327c',
     justifyContent: 'center',
     bottom: 50,
   },
@@ -205,15 +245,15 @@ const styles = StyleSheet.create({
   buttonProfile: {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor:'white',
+    backgroundColor: 'white',
   },
   profilePictureCustomize: {
-    borderRadius: 80,
-    width: 200,
-    height: 200,
+    borderRadius: 50,
+    width: 170,
+    height: 170,
     backgroundColor: '#666666',
     justifyContent: 'center',
-    bottom: 50,
+    marginTop:20,
   },
   idModalProfile: {
     width: 150,
@@ -229,18 +269,79 @@ const styles = StyleSheet.create({
     right: 10,
   },
   textModalProfile: {
-    textAlign:'left',
-    alignSelf:'flex-start',
-    justifyContent:'flex-start',
-    paddingLeft:8,
+    textAlign: 'left',
+    alignSelf: 'flex-start',
+    justifyContent: 'flex-start',
+    paddingLeft: 8,
   },
-  backgroundButtons: {
-    width:300,
-    height:300,
-    backgroundColor:'black',
-  }
+  nameProfile:{
+    fontWeight:'bold',
+    fontSize:20,
+    marginTop:10,
+  },
+  nicknameProfile: {
+    color:'#858585',
+    marginBottom:20,
+  },
+  individualButton: {
+    width: 380,
+    height:90,
+    padding: 25,
+    borderWidth: 1,
+    borderColor: '#e2e2e2',
+    flexDirection: 'row',
+    alignItems:'flex-start',
+    backgroundColor:'white'
+  },
+  textButtons: {
+    fontWeight: 'bold',
+    fontSize:20,
+    bottom:5,
+  },
+  subtitleButton: {
+    color: 'grey',
+    fontSize:12,
+    bottom:10,
+  },
+  iconButton: {
+    width: 40,
+    height: 40,
+    borderWidth:1,
+    borderColor: '#e2e2e2',
+    borderRadius:5,
+    marginRight:10,
+    alignSelf:'center',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  iconArrowButton: {
+    width: 40,
+    height: 40,
+    top:8,
+    alignSelf:'center',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  backgroundResetButton:{
+    marginTop:30,
+    width: 380,
+    height:120,
+    backgroundColor:'white',
+    borderTopEndRadius:30,
+    borderTopStartRadius:30,
+    justifyContent:'center',
+
+  },
+  resetButton: {
+    width:340,
+    height:70,
+    borderRadius:25,
+    alignSelf:'center',
+    borderWidth:1,
+    borderColor:'black',
+    alignItems:'center',
+    justifyContent:'center',
+  },
 });
 
 export default App;
-
-
