a2c20a6383d93f2bd45ce47a46aafdae30783734
Emanuelle Viana <93949785+emanuelleaviana@users.noreply.github.com>
2022-10-30 20:52:55 -0300

    updateHomeScreen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { TextInput } from 'react-native-gesture-handler';
 import { FontAwesome, FontAwesome5, Entypo, AntDesign, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { BlurView, VibrancyView } from "@react-native-community/blur";
 
 
 function HomeScreen({ navigation }) {
@@ -22,21 +23,20 @@ function HomeScreen({ navigation }) {
               setModalVisible(!modalVisible);
             }}
           >
-            <View style={styles.idModal}>
-              <TextInput placeholder='ID Github' style={styles.textInput}></TextInput>
-              <TouchableOpacity onPress={() => navigation.navigate('Profile')} style={styles.buttonStart}>
-                <Text style={styles.textModal}>
-                  <FontAwesome style={styles.iconSearch} name="search" size={20} color="#341933" />
-                </Text>
-              </TouchableOpacity>
-            </View>
-
+            <View style={styles.backgroundColorModal}>
+              <View style={styles.idModal}>
+                <TextInput placeholder='Insira o seu ID Github =)' style={styles.textInput}></TextInput>
+                <TouchableOpacity onPress={() => navigation.navigate('Profile')} style={styles.buttonStart}>
+                  <Text style={styles.textModal}>
+                    <AntDesign name="checksquare" size={33} color="black" />
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              </View>
           </Modal>
           <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.modal}>
             <FontAwesome style={styles.iconSearch} name="search" size={24} color="white" />
           </TouchableOpacity>
-
-
         </View>
         <Text style={styles.textInitial}>Clique na lupa e escreva o nome do usuário (id) do Github que você deseja consultar!</Text>
       </View><AntDesign style={styles.iconGitHub} name="github" size={24} color="black" />
@@ -57,6 +57,7 @@ function Profile({ navigation }) {
           transparent={true}
           visible={modalVisible}
           onRequestClose={() => {
+            r
             setModalVisible(!modalVisible);
           }}
         >
@@ -66,7 +67,6 @@ function Profile({ navigation }) {
               <Text style={styles.textModalProfile}>Id:</Text>
             </TouchableOpacity>
           </View>
-
         </Modal>
         <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.modal}>
           <FontAwesome style={styles.iconSearch} name="search" size={28} color="white" />
@@ -77,13 +77,13 @@ function Profile({ navigation }) {
       <Text style={styles.nicknameProfile}>@emanuelleaviana</Text>
 
       <View>
-        <TouchableOpacity style={[styles.individualButton, {borderTopStartRadius:20, borderTopEndRadius:20}]} onPress={() => navigation.navigate('Bio')}>
+        <TouchableOpacity style={[styles.individualButton, { borderTopStartRadius: 20, borderTopEndRadius: 20 }]} onPress={() => navigation.navigate('Bio')}>
           <View style={styles.iconButton}><FontAwesome5 name="user" size={28} color="black" /></View>
           <View>
             <Text style={styles.textButtons}>Bio</Text>
             <Text style={styles.subtitleButton}>Um pouco sobre o usuário</Text>
           </View>
-          <Text style={[styles.iconArrowButton, {marginLeft:100}]}> <AntDesign name="right" size={24} color="black" /> </Text>
+          <Text style={[styles.iconArrowButton, { marginLeft: 100 }]}> <AntDesign name="right" size={24} color="black" /> </Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.individualButton} onPress={() => navigation.navigate('Orgs')}>
@@ -94,7 +94,7 @@ function Profile({ navigation }) {
             <Text style={styles.textButtons}>Orgs</Text>
             <Text style={styles.subtitleButton}>Organizações que o usuário faz parte</Text>
           </View>
-          <Text style={[styles.iconArrowButton, {marginLeft:30}]}> <AntDesign name="right" size={24} color="black" /> </Text>
+          <Text style={[styles.iconArrowButton, { marginLeft: 30 }]}> <AntDesign name="right" size={24} color="black" /> </Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.individualButton} onPress={() => navigation.navigate('Repositórios')}>
@@ -103,25 +103,25 @@ function Profile({ navigation }) {
             <Text style={styles.textButtons}>Repositórios</Text>
             <Text style={styles.subtitleButton}>Lista contendo todos os repositórios</Text>
           </View>
-          <Text style={[styles.iconArrowButton, {marginLeft:32}]}> <AntDesign name="right" size={24} color="black" /> </Text>
+          <Text style={[styles.iconArrowButton, { marginLeft: 32 }]}> <AntDesign name="right" size={24} color="black" /> </Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={[styles.individualButton, {borderBottomStartRadius:20, borderBottomEndRadius:20}]} onPress={() => navigation.navigate('Seguidores')}>
+        <TouchableOpacity style={[styles.individualButton, { borderBottomStartRadius: 20, borderBottomEndRadius: 20 }]} onPress={() => navigation.navigate('Seguidores')}>
           <View style={styles.iconButton}><MaterialCommunityIcons name="face-recognition" size={28} color="black" /></View>
           <View>
             <Text style={styles.textButtons}>Seguidores</Text>
             <Text style={styles.subtitleButton}>Lista de seguidores</Text>
           </View>
-          <Text style={[styles.iconArrowButton, {marginLeft:138}]}> <AntDesign name="right" size={24} color="black" /> </Text>
+          <Text style={[styles.iconArrowButton, { marginLeft: 138 }]}> <AntDesign name="right" size={24} color="black" /> </Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.backgroundResetButton}>
         <TouchableOpacity style={styles.resetButton}>
-          <Text style={{fontSize:16}}> <Entypo name="log-out" size={20} color="black" /> Resetar</Text>
+          <Text style={{ fontSize: 16 }}> <Entypo name="log-out" size={20} color="black" /> Resetar</Text>
         </TouchableOpacity>
       </View>
-      
+
     </View>
 
 
@@ -219,23 +219,27 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     bottom: 10,
   },
+  backgroundColorModal: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'rgba(0,0,0,0.6)',
+    
+  },
   idModal: {
-    width: 190,
+    width: 300,
     height: 100,
-    backgroundColor: '#86626E',
-    borderTopRightRadius: 30,
-    borderBottomLeftRadius: 30,
-    borderTopLeftRadius: 30,
+    borderRadius:20,
+    backgroundColor: '#50327c',
     flexDirection: 'row',
     alignSelf: 'center',
     justifyContent: 'center',
     alignItems: 'center',
-    top: 300,
-    right: 30,
   },
   textInput: {
     borderWidth: 1,
-    width: 80,
+    width: 200,
+    borderRadius:10,
     backgroundColor: 'white',
     paddingHorizontal: 6,
   },
@@ -253,7 +257,7 @@ const styles = StyleSheet.create({
     height: 170,
     backgroundColor: '#666666',
     justifyContent: 'center',
-    marginTop:20,
+    marginTop: 20,
   },
   idModalProfile: {
     width: 150,
@@ -274,74 +278,74 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     paddingLeft: 8,
   },
-  nameProfile:{
-    fontWeight:'bold',
-    fontSize:20,
-    marginTop:10,
+  nameProfile: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 10,
   },
   nicknameProfile: {
-    color:'#858585',
-    marginBottom:20,
+    color: '#858585',
+    marginBottom: 20,
   },
   individualButton: {
     width: 380,
-    height:90,
+    height: 90,
     padding: 25,
     borderWidth: 1,
     borderColor: '#e2e2e2',
     flexDirection: 'row',
-    alignItems:'flex-start',
-    backgroundColor:'white'
+    alignItems: 'flex-start',
+    backgroundColor: 'white'
   },
   textButtons: {
     fontWeight: 'bold',
-    fontSize:20,
-    bottom:5,
+    fontSize: 20,
+    bottom: 5,
   },
   subtitleButton: {
     color: 'grey',
-    fontSize:12,
-    bottom:10,
+    fontSize: 12,
+    bottom: 10,
   },
   iconButton: {
     width: 40,
     height: 40,
-    borderWidth:1,
+    borderWidth: 1,
     borderColor: '#e2e2e2',
-    borderRadius:5,
-    marginRight:10,
-    alignSelf:'center',
-    justifyContent:'center',
-    alignItems:'center',
+    borderRadius: 5,
+    marginRight: 10,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   iconArrowButton: {
     width: 40,
     height: 40,
-    top:8,
-    alignSelf:'center',
-    justifyContent:'center',
-    alignItems:'center',
+    top: 8,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  backgroundResetButton:{
-    marginTop:30,
+  backgroundResetButton: {
+    marginTop: 30,
     width: 380,
-    height:120,
-    backgroundColor:'white',
-    borderTopEndRadius:30,
-    borderTopStartRadius:30,
-    justifyContent:'center',
+    height: 120,
+    backgroundColor: 'white',
+    borderTopEndRadius: 30,
+    borderTopStartRadius: 30,
+    justifyContent: 'center',
 
   },
   resetButton: {
-    width:340,
-    height:70,
-    borderRadius:25,
-    alignSelf:'center',
-    borderWidth:1,
-    borderColor:'black',
-    alignItems:'center',
-    justifyContent:'center',
-    
+    width: 340,
+    height: 70,
+    borderRadius: 25,
+    alignSelf: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+
   },
 });
 
